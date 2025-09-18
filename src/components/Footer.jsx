adf1f217a54ee7069643e3a471239fef04de9ce4
ASNProject <asnproject02@gmail.com>
2025-09-19 00:23:23 +0700

    create project

---
+++ src/components/Footer.jsx
@@ -0,0 +1,44 @@
+// Copyright 2025 ariefsetyonugroho
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import DataImage from "../data";
+
+const Footer = () => {
+  return (
+    <div className='mt-32 py-4 flex md:flex-row flex-col gap-6 md:gap-0 justify-between items-center'>
+        <img src={DataImage.HeroImage} alt="Hero Image" className="w-10 rounded-md" loading="lazy" />
+        <div className='flex gap-7'>
+            <a href="#beranda">Beranda</a>
+            <a href="#tentang">Tentang</a>
+            <a href="#proyek">Proyek</a>
+        </div>
+        <div className='flex items-center gap-3'>
+            <a href="#">
+                <i className='ri-github-fill ri-2x'></i>
+            </a>
+            <a href="#">
+                <i className='ri-instagram-fill ri-2x'></i>
+            </a>
+            <a href="#">
+                <i className='ri-tiktok-fill ri-2x'></i>
+            </a>
+            <a href="#">
+                <i className='ri-youtube-fill ri-2x'></i>
+            </a>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+// Copyright 2025 ariefsetyonugroho
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+import { useState, useEffect } from "react"
+
+const Navbar = () => {
+    const [active, setActive] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 150) {
+                setActive(true);
+            } else {
+                setActive(false);
+            }
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    return (
+        <div className="navbar py-7 flex items-center justify-between">
+            <div className="logo">
+                <h1 className="text-3xl font-bold bg-white text-black p-1 md:bg-transparent md:text-white">Panzer Robotics</h1>
+            </div>
+            <ul className={`menu flex items-center sm:gap-10 gap-4 md:static fixed left-1/2 -translate-x-1/2 md:translate-x-0 md:opacity-100 md:top-0 bg-white/30 backdrop-blur-md p-4 rounded-br-2xl rounded-bl-2xl md:bg-transparent transition-all md:transition-none z-40 ${active ? "top-0 opacity-100" : "-top-10 opacity-0"}`}>
+                <li><a href="#beranda" className="sm:text-lg text-base font-medium">Beranda</a></li>
+                <li><a href="#tentang" className="sm:text-lg text-base font-medium">Tentang</a></li>
+                <li><a href="#proyek" className="sm:text-lg text-base font-medium">Proyek</a></li>
+                <li><a href="#kontak" className="sm:text-lg text-base font-medium">Kontak</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
