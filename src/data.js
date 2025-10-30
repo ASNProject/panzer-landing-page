eea38a1227586975b430a0e9757463d6f5fd82d3
ASNProject <asnproject02@gmail.com>
2025-10-30 14:39:18 +0700

    update tools item

--- src/data.js
+++ src/data.js
@@ -21,6 +21,18 @@ import Tools8 from "/assets/tools/github.png";
 import Tools9 from "/assets/tools/ai.png";
 import Tools10 from "/assets/tools/canva.png";
 import Tools11 from "/assets/tools/figma.png";
+import Tools12 from "/assets/tools/arduino.png";
+import Tools13 from "/assets/tools/c.png";
+import Tools14 from "/assets/tools/flutter.png";
+import Tools15 from "/assets/tools/laravel.png";
+import Tools16 from "/assets/tools/proteus.png";
+import Tools17 from "/assets/tools/python.png";
+import Tools18 from "/assets/tools/raspberry.png";
+import Tools19 from "/assets/tools/sketchup.png";
+import Tools20 from "/assets/tools/solidworks.png";
+import Tools21 from "/assets/tools/tensorflow.png";
+import Tools22 from "/assets/tools/firebase.png";
+import Tools23 from "/assets/tools/mysql.png";
 
 export const listTools = [
   {
@@ -32,73 +44,150 @@ export const listTools = [
   },
   {
     id: 2,
-    gambar: Tools2,
-    nama: "React JS",
-    ket: "Framework",
+    gambar: Tools12,
+    nama: "Arduino",
+    ket: "Microcontroller",
     dad: "200",
   },
   {
     id: 3,
-    gambar: Tools3,
-    nama: "Next JS",
+    gambar: Tools14,
+    nama: "Flutter",
     ket: "Framework",
     dad: "300",
   },
   {
     id: 4,
-    gambar: Tools4,
-    nama: "Tailwind CSS",
+    gambar: Tools15,
+    nama: "Laravel",
     ket: "Framework",
     dad: "400",
   },
   {
     id: 5,
-    gambar: Tools5,
-    nama: "Bootstrap",
-    ket: "Framework",
+    gambar: Tools16,
+    nama: "Proteus",
+    ket: "Desain & PCB Simulator",
     dad: "500",
   },
   {
     id: 6,
-    gambar: Tools6,
-    nama: "Javascript",
-    ket: "Language",
+    gambar: Tools13,
+    nama: "C++",
+    ket: "Lenguage",
     dad: "600",
   },
   {
     id: 7,
-    gambar: Tools7,
-    nama: "Node JS",
-    ket: "Javascript Runtime",
+    gambar: Tools17,
+    nama: "Python",
+    ket: "Language",
     dad: "700",
   },
   {
     id: 8,
-    gambar: Tools8,
-    nama: "Github",
-    ket: "Repository",
+    gambar: Tools18,
+    nama: "Raspberry",
+    ket: "Mini Computer",
     dad: "800",
   },
   {
     id: 9,
-    gambar: Tools9,
-    nama: "Adobe Illustrator",
-    ket: "Design App",
+    gambar: Tools19,
+    nama: "Sketchup",
+    ket: "Desain App",
     dad: "900",
   },
   {
     id: 10,
-    gambar: Tools10,
-    nama: "Canva",
-    ket: "Design App",
+    gambar: Tools20,
+    nama: "Solidworks",
+    ket: "Desain App",
     dad: "1000",
   },
   {
     id: 11,
+    gambar: Tools21,
+    nama: "Tensorflow",
+    ket: "Framework AI",
+    dad: "1100",
+  },
+  {
+    id: 12,
+    gambar: Tools2,
+    nama: "React JS",
+    ket: "Framework",
+    dad: "1200",
+  },
+  {
+    id: 13,
+    gambar: Tools3,
+    nama: "Next JS",
+    ket: "Framework",
+    dad: "1300",
+  },
+  {
+    id: 14,
+    gambar: Tools4,
+    nama: "Tailwind CSS",
+    ket: "Framework",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools5,
+    nama: "Bootstrap",
+    ket: "Framework",
+    dad: "1500",
+  },
+  {
+    id: 16,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "1600",
+  },
+  {
+    id: 17,
+    gambar: Tools7,
+    nama: "Node JS",
+    ket: "Javascript Runtime",
+    dad: "1700",
+  },
+  {
+    id: 18,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "1800",
+  },
+  {
+    id: 19,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1900",
+  },
+  {
+    id: 20,
     gambar: Tools11,
     nama: "Figma",
     ket: "Design App",
-    dad: "1100",
+    dad: "2000",
+  },
+  {
+    id: 21,
+    gambar: Tools22,
+    nama: "Firebase",
+    ket: "Cloud Service",
+    dad: "2100",
+  },
+  {
+    id: 22,
+    gambar: Tools23,
+    nama: "MySQL",
+    ket: "Database",
+    dad: "2200",
   },
 ];
 
